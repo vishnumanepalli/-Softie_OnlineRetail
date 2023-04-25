36f175cfb9769eab6c23a19a911840d3ecdaaf57
JyotiDhiman3 <2020csb1092@iitrpr.ac.in>
2023-04-26 03:02:12 +0530

    j1

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Navb from './design/navbar';
+import SearchBar from './searchbar';
+import Filters from './filters';
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    // Perform the search logic or API call here
+  };
+
   return (
     <div className="App">
-      <Navb/>
+      <Navb />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            <Filters />
+          </div>
+          <div className="col-md-9">
+            {/* Render products or search results here */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/design/navbar.js
+++ client/src/design/navbar.js
@@ -4,25 +4,27 @@ import Navbar from 'react-bootstrap/Navbar';
 import '../App.css';
 import Img1 from './img.png';
 import Img2 from './android-chrome-192x192.png';
+import SearchBar from '../searchbar';
 
 function Navb() {
-    return (
-        <>
-        <Navbar className="custom-navbar">
-          <Container>
-            <img src={Img2} alt="cart"  style={{ width: "50px", height: "auto" }}/>
-            <Navbar.Brand href="#home">GroceryStore</Navbar.Brand>
-            <Nav className="me-auto custom-nav-links">
-              <Nav.Link href="#Product" className="custom-nav-link">Products</Nav.Link>
-              <Nav.Link href="#Login" className="custom-nav-link">Login</Nav.Link>
-              <Nav.Link href="#cart" className="custom-nav-link">
-                <img src={Img1} alt="cart" className="shopping-cart-icon" />
-              </Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-        </>
-      );
-  }
+  return (
+      <>
+      <Navbar className="custom-navbar">
+        <Container>
+          <img src={Img2} alt="cart"  style={{ width: "50px", height: "auto" }}/>
+          <Navbar.Brand href="#home">GroceryStore</Navbar.Brand>
+          <Nav className="me-auto custom-nav-links">
+          <SearchBar />
+            <Nav.Link href="#Product" className="custom-nav-link">Products</Nav.Link>
+            <Nav.Link href="#Login" className="custom-nav-link">Login</Nav.Link>
+            <Nav.Link href="#cart" className="custom-nav-link">
+              <img src={Img1} alt="cart" className="shopping-cart-icon" />
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      </>
+    );
+}
 
 export default Navb;
