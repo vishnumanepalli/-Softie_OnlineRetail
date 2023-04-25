3cdcdf64af37617918bdedea5c704c7aa1cf493b
vishnumanepalli <2020csb1097@iitrpr.ac.in>
2023-04-26 04:09:49 +0530

    products done

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import '../App.css';
-const Products= () => {
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const response = await axios.post('http://localhost:5000/get_products');
+    setProducts(response.data);
+  }
+
+  const addToCart = async (productId) => {
+    const response = await axios.post('http://localhost:5000/cart', { product_id: productId });
+    console.log(response.data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div className='home'>
+    <div>
       <h1>Products</h1>
+      <br/>
+      <div className='product-list'>
+        {products.map(product => (
+          <div className='product-card' key={product.product_id}>
+            <img src={product.image_url} alt={product.name} className='product-image' />
+            <h2>{product.name}</h2>
+            <p>{product.price}</p>
+            <button onClick={() => addToCart(product.product_id)}>Add to Cart</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
