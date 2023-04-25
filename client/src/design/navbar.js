152dd1e9e69ad55100965142fc085341fad4e06b
Sreya Gopagoni <2020csb1087@iitrpr.ac.in>
2023-04-25 18:34:41 +0530

    s3

--- client/src/design/navbar.js
+++ client/src/design/navbar.js
@@ -13,9 +13,9 @@ function Navb() {
             <img src={Img2} alt="cart"  style={{ width: "50px", height: "auto" }}/>
             <Navbar.Brand href="#home">GroceryStore</Navbar.Brand>
             <Nav className="me-auto custom-nav-links">
-              <Nav.Link href="#home" className="custom-nav-link">Products</Nav.Link>
-              <Nav.Link href="#products" className="custom-nav-link">Login</Nav.Link>
-              <Nav.Link href="#login" className="custom-nav-link">
+              <Nav.Link href="#Product" className="custom-nav-link">Products</Nav.Link>
+              <Nav.Link href="#Login" className="custom-nav-link">Login</Nav.Link>
+              <Nav.Link href="#cart" className="custom-nav-link">
                 <img src={Img1} alt="cart" className="shopping-cart-icon" />
               </Nav.Link>
             </Nav>
