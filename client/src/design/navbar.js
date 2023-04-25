ce56e0a1cb9f75fec1c411127bdcae1957996eaf
Sreya Gopagoni <2020csb1087@iitrpr.ac.in>
2023-04-25 18:28:28 +0530

    s3

--- client/src/design/navbar.js
+++ client/src/design/navbar.js
@@ -2,17 +2,22 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../App.css';
+import Img1 from './img.png';
+import Img2 from './android-chrome-192x192.png';
 
 function Navb() {
     return (
         <>
         <Navbar className="custom-navbar">
           <Container>
+            <img src={Img2} alt="cart"  style={{ width: "50px", height: "auto" }}/>
             <Navbar.Brand href="#home">GroceryStore</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Products</Nav.Link>
-              <Nav.Link href="#features">Login</Nav.Link>
-              <Nav.Link href="#pricing"><img src="./img.png" alt="Cart" /></Nav.Link>
+            <Nav className="me-auto custom-nav-links">
+              <Nav.Link href="#home" className="custom-nav-link">Products</Nav.Link>
+              <Nav.Link href="#products" className="custom-nav-link">Login</Nav.Link>
+              <Nav.Link href="#login" className="custom-nav-link">
+                <img src={Img1} alt="cart" className="shopping-cart-icon" />
+              </Nav.Link>
             </Nav>
           </Container>
         </Navbar>
