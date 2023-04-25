c8a26ffb4f99808f85f69a13905f93d1b72020fe
Sreya Gopagoni <2020csb1087@iitrpr.ac.in>
2023-04-26 02:53:16 +0530

    s4

--- client/src/design/navbar.js
+++ client/src/design/navbar.js
@@ -11,11 +11,11 @@ function Navb() {
         <Navbar className="custom-navbar">
           <Container>
             <img src={Img2} alt="cart"  style={{ width: "50px", height: "auto" }}/>
-            <Navbar.Brand href="#home">GroceryStore</Navbar.Brand>
+            <Navbar.Brand href="/">GroceryStore</Navbar.Brand>
             <Nav className="me-auto custom-nav-links">
-              <Nav.Link href="#Product" className="custom-nav-link">Products</Nav.Link>
-              <Nav.Link href="#Login" className="custom-nav-link">Login</Nav.Link>
-              <Nav.Link href="#cart" className="custom-nav-link">
+              <Nav.Link href="/Products" className="custom-nav-link">Products</Nav.Link>
+              <Nav.Link href="/Login" className="custom-nav-link">Login</Nav.Link>
+              <Nav.Link href="/cart" className="custom-nav-link">
                 <img src={Img1} alt="cart" className="shopping-cart-icon" />
               </Nav.Link>
             </Nav>
@@ -25,4 +25,4 @@ function Navb() {
       );
   }
 
-export default Navb;
+  export default Navb;
