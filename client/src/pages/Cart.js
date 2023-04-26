4b76bd965afe8e87f1b0414b967ba8e0acd90ed9
Sreya Gopagoni <2020csb1087@iitrpr.ac.in>
2023-04-27 02:33:40 +0530

    s1

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../css/cart.css';
 
 const Cart = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    // Fetch the cart items from the server
+    axios.post('http://localhost:5000/get_cartitems', { cartId: 1 })
+      .then(res => {
+        setCartItems(res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <>
-      <h1>Cart</h1>
-    </>
+    <div className="cart-container">
+    <table className="cart-table">
+      <thead>
+        <tr>
+          <th>Product Name</th>
+          <th>Price</th>
+          <th>Image</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cartItems.map(item => (
+          <tr key={item.product_id}>
+            <td>{item.name}</td>
+            <td>${item.price.toFixed(2)}</td>
+            <td><img className="cart-image" src={item.image_url} alt={item.name} /></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
   );
-};
+}
 
 export default Cart;
