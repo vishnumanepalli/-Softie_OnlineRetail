7ab85ba2d2a55507155d585c526eaae86335513e
JyotiDhiman3 <2020csb1092@iitrpr.ac.in>
2023-04-27 02:32:25 +0530

    j1

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import '../css/cart.css';
 
 const Cart = () => {
   return (
-    <>
-      <h1>Cart</h1>
-    </>
+    <div className="cart-container">
+      <h1 className="cart-title">Cart</h1>
+        <div className="cart-item">
+          <img src="https://hsnbazar.com/images/empty-cart.png" alt="cart is empty" />
+            <button className="continue-shopping-button">Continue Shopping!</button>
+          </div>
+        </div>
   );
-};
+}
 
 export default Cart;
