3c7f8a3f0e5e7705c90c7f547cdf06d11cdea4a2
Sreya Gopagoni <2020csb1087@iitrpr.ac.in>
2023-05-01 23:47:43 +0530

    s1

--- client/src/design/filters.js
+++ client/src/design/filters.js
@@ -23,21 +23,14 @@ const Filters = (props) => {
         <ul className="category-list">
           <li><span onClick={() => handleCategoryClick('vegetables')}>Vegetables</span></li>
           <li><span onClick={() => handleCategoryClick('Fruits')}>Fruits</span></li>
-          <li><span onClick={() => handleCategoryClick('Sports,Toys & Luggage')}>Sports,Toys & Luggage</span></li>
+          <li><span onClick={() => handleCategoryClick('Clothing')}>Clothing</span></li>
+          <li><span onClick={() => handleCategoryClick('Sports')}>Sports</span></li>
+          <li><span onClick={() => handleCategoryClick('Toys')}>Toys </span></li>
+          <li><span onClick={() => handleCategoryClick('Stationary')}>Stationary</span></li>
           <li><span onClick={() => handleCategoryClick('Grocery')}>Grocery</span></li>
+          <li><span onClick={() => handleCategoryClick('home')}>home</span></li>
           <li><span onClick={() => handleCategoryClick('Beauty')}>Beauty</span></li>
-          <li><span onClick={() => handleCategoryClick('Furniture')}>Furniture</span></li>
         </ul>
-      </div>
-      <div className="price-range-container">
-        <h6>Price Range</h6>
-        <div className="price-range-slider">
-          <input type="range" name="price" min="0" max="10000" step="10" value={priceRange[0]} onChange={handlePriceChange} />
-          <input type="range" name="price" min="0" max="10000" step="10" value={priceRange[1]} onChange={handlePriceChange} />
-        </div>
-        <div className="price-range-display">
-          <p>Price: ₹{priceRange[0]} - ₹{priceRange[1]}</p>
-        </div>
       </div>
     </div>
   );
