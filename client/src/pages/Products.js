f2b3b3e8b59cddc5f5d67b7e655b553e247011df
Tanish Goyal <92181886+Tancoder20@users.noreply.github.com>
2023-05-01 06:26:14 +0530

    updated products.js

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,125 +1,69 @@
-import Filters from '../design/filters';
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import '../css/product.css';
-import { useNavigate} from 'react-router-dom';
+import '../css/viewproduct.css';
 import {useCookies} from 'react-cookie'
 
-const Products = () => {
+const Viewproduct = () => {
   const [cookies,setCookie,removeCookie] = useCookies(null);
-  const [products, setProducts] = useState([]);
-  const [wishlistItems, setWishlistItems] = useState([]);
-  const navigate = useNavigate();
-  const fetchProducts = async () => {
-    const response = await axios.post('http://localhost:5000/get_products');
-    setProducts(response.data);
-    axios.post('http://localhost:5000/get_wishlist', { userId:  cookies.userId })
-      .then(res => {
-        setWishlistItems(res.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-  
-  const addToWishlist= async (productId) =>{
-    console.log(cookies.userId);
-
-    var server_address = 'http://localhost:5000/add_to_wishlist';
-    const resp = await fetch(server_address, {
-      method: "POST",
-      headers: { "Content-Type": "application/json", 
-      "jwt-token" : localStorage.getItem("token"), },
-      body: JSON.stringify({ 
-        userId:cookies.userId,
-        productId:productId
-       }),
-    });
-    
-    const response = await resp.json();
-    console.log("Server response", response);
-    axios.post('http://localhost:5000/get_wishlist', { userId:  cookies.userId })
-      .then(res => {
-        setWishlistItems(res.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  const addToCart = async (productId) => {
-    console.log(1)
-
-    var server_address = 'http://localhost:5000/cart';
-    const resp2 = await fetch(server_address, {
-      method: "POST",
-      headers: { "Content-Type": "application/json", 
-      "jwt-token" : localStorage.getItem("token"), },
-      body: JSON.stringify({ 
-        user_id: cookies.userId,
-        product_id:productId
-       }),
-    });
-   
-    const response = await resp2.json();
-    console.log("Server response", response);
-
-  }
-  const navigateToProductdetails = (productId) => {
-    navigate(`/Products/${productId}`);
-  };
-
-  const removeFromWishlist = async (product_id) => {
-    const response = await axios.delete('http://localhost:5000/delete_from_wishlist', { 
-      data: { 
-        userId:  cookies.userId, 
-        productId: product_id 
-      } 
-    });
-    axios.post('http://localhost:5000/get_wishlist', { userId:  cookies.userId })
-      .then(res => {
-        setWishlistItems(res.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  const toggleWishlist = (productId) => {
-    if (wishlistItems.some(item => item.product_id === productId)) {
-      removeFromWishlist(productId);
-    } else {
-      addToWishlist(productId);
-    }
-  }
-  
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
 
   useEffect(() => {
-    fetchProducts();
-  }, []);
+    axios.post('http://localhost:5000/get_productdetails', { productId: id })
+      .then(response => setProduct(response.data))
+      .catch(error => console.error(error));
+  }, [id]);
 
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+
+
+  const addToCart = async () => {
+    const response = await axios.post('http://localhost:5000/cart', { user_id:cookies.userId , product_id: id });
+    console.log(response.data);
+  }
+  
   return (
-    <div>
-      <Filters />
-      <br/>
-      <div className='product-list'>
-        {products.map(product => (
-          <div className='product-card' key={product.product_id}>
-            <img src={product.image_url} alt={product.name} className='product-image-hp'  onClick={() => navigateToProductdetails(product.product_id)} />
-            <h2  onClick={() => navigateToProductdetails(product.product_id)}>{product.name}</h2>
-            <p  onClick={() => navigateToProductdetails(product.product_id)}>₹{product.price}</p>
-            <div className='button-container'>
-              <button className='add-to-cart-button' onClick={() => addToCart(product.product_id,product.name)}>Add to Cart</button>
-              <button className='wishlist-button' onClick={() => toggleWishlist(product.product_id)}>
-                {wishlistItems.some(item => item.product_id === product.product_id) ? '❤️' : '🤍'}
-              </button>
-
-            </div>
-          </div>
-        ))}
+    <div className="view-product">
+      <img src={product.image_url} alt={product.name} className='product-image' />
+      <div className="product-details">
+        <h1 className="product-title">{product.name}</h1>
+        <p className="product-description">{product.description}</p>
+        <p className="product-price">Price: ₹{product.price}</p>
+        <button className="add-to-cart" onClick={() => addToCart(product.product_id,product.name)}>Add to Cart</button>
       </div>
     </div>
   );
 };
 
-export default Products;
+export default Viewproduct;
+export function displaySearchResults(searchResults) {
+  const searchResultsDiv = document.getElementById('searchResults');
+  searchResultsDiv.innerHTML = '';
+
+  for (let i = 0; i < searchResults.length; i++) {
+    const result = searchResults[i];
+    const resultDiv = document.createElement('div');
+    resultDiv.innerHTML = `<h3>${result.name}</h3>
+                            <p>${result.description}</p>
+                            <p>${result.price}</p>`;
+    resultDiv.onclick = () => {
+      displayProductDetails(result.id);
+    };
+    searchResultsDiv.appendChild(resultDiv);
+  }
+}
+
+export function displayProductDetails(productId) {
+  axios.get(`/get_productdetails?id=${productId}`)
+    .then(response => {
+      const productDetailsDiv = document.getElementById('productDetails');
+      productDetailsDiv.innerHTML = `<h3>${response.data.name}</h3>
+                                      <p>${response.data.description}</p>
+                                      <p>${response.data.price}</p>`;
+    })
+    .catch(error => console.error(error));
+}
+
